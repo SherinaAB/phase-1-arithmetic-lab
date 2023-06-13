bf49ad8b12db6b9ed692f78b43e479a9136d144a
Sherina Buenaseda <sherina76@gmail.com>
2023-06-13 02:32:10 -0600

    added Maths

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+
+    let num1 = 31;
+    let num2 = 2;
+    let multiply = num1 * num2;
+    console.log(multiply);
+
+    let random = Math.floor(Math.random())+1;
+    console.log(random);
+
+    let num3 = 20;
+    let num4 = 8;
+    let mod = num3%num4
+    console.log(mod);
+    
+    
+    let max = (4,8,12,16,20);
+    let highestNumber = []
+     Math.max(max);
+        console.log (highestNumber)
